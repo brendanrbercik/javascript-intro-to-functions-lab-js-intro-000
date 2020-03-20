779ef26b4bd6d78f01efca6aae722d977b394b19
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-20 15:14:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  string === lowercase ?
-  "I can't hear you!" :
-  "I love you, too."
+  if(string === uppercase) {
+    return "YES INDEED!"
+  }
+  else if (string === lowercase){
+    return "I can't hear you!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
