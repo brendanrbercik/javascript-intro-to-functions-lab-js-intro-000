1a9598be633be657980e24fc5382c06b8ec14f03
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-20 15:07:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  return string.toLowerCase(stringShout)
-  console.log(stringShout)
+  return string.toLowerCase(stringShout);
+  console.log(stringShout);
 }
